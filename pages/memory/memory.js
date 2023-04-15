9bcef47d5ffcc58647796e6a39f969c4a4103500
April <641676557@qq.com>
2023-04-15 22:08:16 +0800

    记忆设置

--- pages/memory/memory.js
+++ pages/memory/memory.js
@@ -1,22 +1,114 @@
 // pages/memory/memory.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    dailycount: 10,
-    familiarday: 4,
-    blurday: 2,
-    strangeday: 1,
-    show: false
+    count: 4,
+    memoryTrace: [2, 4, 7, 15],
+    showDialog: false,
+    current: 0,
+    num: 0,
+    confirmDelete: false,
+    showAdd: false
+  },
+  toNext() {
+    this.setData({
+      showAdd: true
+    });
+  },
+  goDelete(e) {
+    this.setData({
+      confirmDelete: true,
+      current: e.target.dataset.index + 1
+    })
+  },
+  addFamiliar() {
+    let that = this
+    let arr = this.data.memoryTrace
+    arr.push(this.data.num)
+    // console.log(arr)
+    wx.request({
+      url: 'http://localhost:2346/user/changefamiliar?openid=' + app.globalData.userInfo.id + '&familiar=' + arr.toString(),
+      success: (res) => {
+        console.log(res)
+        that.setData({
+          showDialog: false,
+          showAdd: false,
+          num: 0,
+          memoryTrace: arr
+        })
+      }
+    })
   },
 
+  deleteFamiliar() {
+    let that = this
+    let arr = this.data.memoryTrace
+    arr.splice(this.data.current - 1, 1)
+    wx.request({
+      url: 'http://localhost:2346/user/changefamiliar?openid=' + app.globalData.userInfo.id + '&familiar=' + arr.toString(),
+      success: (res) => {
+        console.log(res)
+        that.setData({
+          showDialog: false,
+          confirmDelete:false,
+          memoryTrace:arr,
+          num: 0
+        })
+      }
+    })
+    console.log(arr)
+  },
+
+  cancelDelete() {
+    this.setData({
+      confirmDelete: false
+    })
+  },
+
+  goEdit(e) {
+    this.setData({
+      showDialog: true,
+      current: e.target.dataset.index + 1
+    })
+  },
+
+  closeDialog() {
+    this.setData({
+      showDialog: false,
+      showAdd: false
+    })
+  },
+
+  changeFamiliar() {
+    let that = this
+    let arr = this.data.memoryTrace
+    arr[this.data.current - 1] = this.data.num
+    this.setData({
+      memoryTrace: arr,
+    })
+    wx.request({
+      url: 'http://localhost:2346/user/changefamiliar?openid=' + app.globalData.userInfo.id + '&familiar=' + this.data.memoryTrace.toString(),
+      success: (res) => {
+        console.log(res)
+        that.setData({
+          showDialog: false,
+          num: 0
+        })
+      }
+    })
+    console.log(this.data.memoryTrace)
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    this.setData({
+      memoryTrace: app.globalData.userInfo.familiar.split(",")
+    })
   },
 
   /**
@@ -66,11 +158,5 @@ Page({
    */
   onShareAppMessage() {
 
-  },
-
-  edit() {
-    this.setData({
-      show: true
-    })
   }
 })
